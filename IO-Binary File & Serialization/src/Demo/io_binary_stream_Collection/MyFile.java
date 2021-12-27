25b52cd9d2b04ee011e2205e8a994c3ea8da64be
TriHoangZXC <hoanghuutri97@gmail.com>
2021-12-27 17:17:58 +0700

    Add Demo IO (binary + text) file

---
+++ IO-Binary File & Serialization/src/Demo/io_binary_stream_Collection/MyFile.java
@@ -0,0 +1,34 @@
+package Demo.io_binary_stream_Collection;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class MyFile {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(-1);
+        integers.add(3);
+        integers.add(4);
+
+        FileOutputStream fileOutputStream = new FileOutputStream("src/Demo/io_binary_stream_Collection/demoFile.txt");
+        DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
+        for (Integer i : integers) {
+            dataOutputStream.writeInt(i);
+        }
+        dataOutputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream("src/Demo/io_binary_stream_Collection/demoFile.txt");
+        DataInputStream dataInputStream = new DataInputStream(fileInputStream);
+
+        try {
+            Integer line;
+            while ((line = dataInputStream.readInt()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println();
+        }
+    }
+}
