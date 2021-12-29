ac5b7e0a95f8334709a15f8989a37fba48a53a09
TriHoangZXC <hoanghuutri97@gmail.com>
2021-12-29 11:45:33 +0700

    Add [Thực hành] Validate email file

---
+++ String & Regex/src/Validate_Email/EmailExampleTest.java
@@ -0,0 +1,18 @@
+package Validate_Email;
+
+public class EmailExampleTest {
+    private static final EmailExample emailExample = new EmailExample();
+    public static final String[] validEmail = new String[]{"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] invalidEmail = new String[]{"@gmail.com", "ab@yahoo.", "@#abc@hotmail.com"};
+
+    public static void main(String[] args) {
+        for (String email : validEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
+        }
+        for (String email : invalidEmail) {
+            boolean isvalid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
+        }
+    }
+}
