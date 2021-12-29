73fddb7e3f9d394e3eee72182b3b800206b5789a
TriHoangZXC <hoanghuutri97@gmail.com>
2021-12-29 23:38:56 +0700

    Add Sửa Bài tập thêm Ngày 29 tháng 12 file

--- Minitest/src/BaiTapThemNgay29Thang12/Main.java
+++ Minitest/src/BaiTapThemNgay29Thang12/Main.java
@@ -1,5 +1,6 @@
 package BaiTapThemNgay29Thang12;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -17,14 +18,14 @@ public class Main {
             System.out.println("5. Hiển thị tất cả sinh viên có điểm trung bình trên 7.5: ");
             System.out.println("6. Hiển thị tất cả sinh viên theo cú pháp: ");
             System.out.println("7. Ghi ra file CSV: ");
+            System.out.println("8. Đọc file CSV: ");
             System.out.println("0. Exit");
             System.out.println("Nhập lựa chọn của bạn: ");
             choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
                 case 1:
-                    Student student = studentManager.creatStudent();
-                    studentManager.addStudent(student);
+                    studentManager.addStudent();
                     break;
                 case 2:
                     System.out.println("Nhập tên sinh viên muốn xóa: ");
@@ -40,38 +41,17 @@ public class Main {
                     studentManager.displayAllStudent();
                     break;
                 case 5:
-                    System.out.println(studentManager.displayAllStudentAvgPointHigherThanSevenDotFive());
+                    studentManager.displayAllStudentAvgPointHigherThanSevenDotFive();
                     break;
                 case 6:
-                    int choice1;
-                    do {
-                        System.out.println("Menu");
-                        System.out.println("1. Hiển thị tất cả sinh viên Giỏi: ");
-                        System.out.println("2. Hiển thị tất cả sinh viên Khá: ");
-                        System.out.println("3. Hiển thị tất cả sinh viên Trung Bình: ");
-                        System.out.println("4. Hiển thị tất cả sinh viên Yếu: ");
-                        System.out.println("0. Exit");
-                        System.out.println("Nhập lựa chọn của bạn: ");
-                        choice1 = scanner.nextInt();
-                        scanner.nextLine();
-                        switch (choice1) {
-                            case 1:
-                                System.out.println(studentManager.displayAllStudentGood());
-                                break;
-                            case 2:
-                                System.out.println(studentManager.displayAllStudentRather());
-                                break;
-                            case 3:
-                                System.out.println(studentManager.displayAllStudentMedium());
-                                break;
-                            case 4:
-                                System.out.println(studentManager.displayAllStudentWeak());
-                                break;
-                        }
-                    } while (choice1 != 0);
+                    studentManager.displayStudentFormat();
                     break;
                 case 7:
-                    studentManager.exportCsv();
+                    studentManager.writeFile(studentManager.getStudentArrayList(), StudentManager.PATH_NAME);
+                    break;
+                case 8:
+                    ArrayList<Student> studentArrayList = studentManager.readFile(StudentManager.PATH_NAME);
+                    studentArrayList.forEach(System.out::println);
                     break;
             }
         } while (choice != 0);
--- Minitest/src/BaiTapThemNgay29Thang12/StudentManager.java
+++ Minitest/src/BaiTapThemNgay29Thang12/StudentManager.java
@@ -1,39 +1,75 @@
 package BaiTapThemNgay29Thang12;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentManager {
-    ArrayList<Student> studentArrayList = new ArrayList<>();
-    Scanner scanner = new Scanner(System.in);
+    public static final String PATH_NAME = "src/BaiTapThemNgay29Thang12/students.csv";
+    private final ArrayList<Student> studentArrayList;
+    private final Scanner scanner = new Scanner(System.in);
 
     public StudentManager() {
-        this.studentArrayList = new ArrayList<>();
+        this.studentArrayList = readFile(PATH_NAME);
     }
 
-    public Student creatStudent() {
+    public ArrayList<Student> getStudentArrayList() {
+        return studentArrayList;
+    }
+
+    public boolean checkAgeAndAverage(int age, double averagePoint) {
+        return (age >= 18 && age <= 60 && averagePoint >= 0 && averagePoint <= 10);
+    }
+
+    public String getGenderByChoice(int choice) {
+        String gender = "";
+        switch (choice) {
+            case 1:
+                gender = "Male";
+                break;
+            case 2:
+                gender = "Female";
+                break;
+            case 3:
+                gender = " Other";
+                break;
+        }
+        return gender;
+    }
+
+    public void addStudent() {
         System.out.println("Nhập tên: ");
         String name = scanner.nextLine();
         System.out.println("Nhập tuổi: ");
         int age = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Nhập giới tính: ");
-        String gender = scanner.nextLine();
+        System.out.println("Chọn giới tính: ");
+        System.out.println("1. Male");
+        System.out.println("2. Female");
+        System.out.println("3. Other");
+        System.out.println("Nhập lựa chọn của bạn: ");
+        int gender = scanner.nextInt();
+        scanner.nextLine();
         System.out.println("Nhập địa chỉ: ");
         String address = scanner.nextLine();
         System.out.println("Nhập điểm trung bình: ");
         double avgPoint = scanner.nextDouble();
         scanner.nextLine();
-        return new Student(name, age, gender, address, avgPoint);
+        if (checkAgeAndAverage(age, avgPoint) && !getGenderByChoice(gender).equals("")) {
+            studentArrayList.add(new Student(name, age, getGenderByChoice(gender), address, avgPoint));
+            writeFile(studentArrayList, PATH_NAME);
+            System.out.println("Thêm sinh viên có tên:  " + name + " thành công!");
+        } else {
+            System.out.println("Lỗi, vui lòng nhập lại!!!");
+        }
     }
 
-    public void addStudent(Student student) {
-        studentArrayList.add(student);
-    }
 
     public void displayAllStudent() {
+        if (studentArrayList.isEmpty()) {
+            System.out.println("Danh sách sinh viên rỗng!");
+            return;
+        }
         for (Student student : studentArrayList) {
             System.out.println(student);
         }
@@ -49,121 +85,117 @@ public class StudentManager {
         }
         if (student != null) {
             studentArrayList.remove(student);
+            writeFile(studentArrayList, PATH_NAME);
+            System.out.println("Xóa sinh viên tên: " + name + " thành công!");
         }
         return student;
     }
 
     public Student editStudentByName(String name) {
-        Student studentUpdate = null;
+        Student studentEdit = null;
         for (Student s : studentArrayList) {
             if (s.getName().equals(name)) {
-                studentUpdate = s;
+                studentEdit = s;
                 break;
             }
         }
-        if (studentUpdate != null) {
-            int index = studentArrayList.indexOf(studentUpdate);
+        if (studentEdit != null) {
+            int index = studentArrayList.indexOf(studentEdit);
             System.out.println("Nhập tên mới: ");
             String newName = scanner.nextLine();
-            studentUpdate.setName(newName);
+            studentEdit.setName(newName);
             System.out.println("Nhập tuổi mới: ");
             int newAge = scanner.nextInt();
-            studentUpdate.setAge(newAge);
+            studentEdit.setAge(newAge);
             scanner.nextLine();
-            System.out.println("Nhập giới tính mới: ");
-            String newGender = scanner.nextLine();
-            studentUpdate.setAddress(newGender);
+            System.out.println("Chọn giới tính mới: ");
+            System.out.println("1. Male");
+            System.out.println("2. Female");
+            System.out.println("3. Other");
+            System.out.println("Nhập lựa chọn của bạn: ");
+            int gender = scanner.nextInt();
+            scanner.nextLine();
             System.out.println("Nhập địa chỉ mới: ");
             String newAdd = scanner.nextLine();
-            studentUpdate.setAddress(newAdd);
+            studentEdit.setAddress(newAdd);
             System.out.println("Nhập điểm trung bình mới: ");
             double newAvgPoint = scanner.nextDouble();
-            studentUpdate.setAveragePoint(newAvgPoint);
+            studentEdit.setAveragePoint(newAvgPoint);
             scanner.nextLine();
-            studentArrayList.set(index, studentUpdate);
+            if (checkAgeAndAverage(studentEdit.getAge(), studentEdit.getAveragePoint()) && !getGenderByChoice(gender).equals("")) {
+                studentEdit.setGender(getGenderByChoice(gender));
+                studentArrayList.set(index, studentEdit);
+                writeFile(studentArrayList, PATH_NAME);
+                System.out.println("Sửa thông tin sinh viên có tên:  " + name + " thành công!");
+            } else {
+                System.out.println("Lỗi, vui lòng nhập lại!!!");
+            }
         }
-        return studentUpdate;
+        return studentEdit;
     }
 
-    public ArrayList<Student> displayAllStudentAvgPointHigherThanSevenDotFive() {
-        ArrayList<Student> students = new ArrayList<>();
+    public void displayAllStudentAvgPointHigherThanSevenDotFive() {
+        if (studentArrayList.isEmpty()) {
+            System.out.println("Danh sách sinh viên rỗng");
+            return;
+        }
         for (Student student : studentArrayList) {
             if (student.getAveragePoint() > 7.5) {
-                students.add(student);
+                System.out.println(student);
             }
         }
-        return students;
     }
 
-    public ArrayList<Student> displayAllStudentGood() {
-        ArrayList<Student> students = new ArrayList<>();
+
+    public void displayStudentFormat() {
+        if (studentArrayList.isEmpty()) {
+            System.out.println("Danh sách sinh viên rỗng!");
+            return;
+        }
         for (Student student : studentArrayList) {
-            if (student.getAveragePoint() > 8) {
-                students.add(student);
+            if (student.getAveragePoint() > 8.0) {
+                System.out.println(student.getName() + " - " + student.getAveragePoint() + " - " + "Good");
+            } else if (student.getAveragePoint() > 6.0) {
+                System.out.println(student.getName() + " - " + student.getAveragePoint() + " - " + "Pretty");
+            } else if (student.getAveragePoint() > 4.0) {
+                System.out.println(student.getName() + " - " + student.getAveragePoint() + " - " + "Normal");
+            } else {
+                System.out.println(student.getName() + " - " + student.getAveragePoint() + " - " + "Weak");
             }
         }
-        return students;
     }
 
-    public ArrayList<Student> displayAllStudentRather() {
-        ArrayList<Student> students = new ArrayList<>();
-        for (Student student : studentArrayList) {
-            if (student.getAveragePoint() <= 8 && student.getAveragePoint() > 6) {
-                students.add(student);
+    public void writeFile(ArrayList<Student> studentArrayList, String path) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path));
+            for (Student student : studentArrayList) {
+                bufferedWriter.write(student.getName() + ", " + student.getAge() + ", " + student.getGender() + ", " +
+                        student.getAddress() + ", " + student.getAveragePoint() + "\n");
             }
-        }
-        return students;
-    }
-
-    public ArrayList<Student> displayAllStudentMedium() {
-        ArrayList<Student> students = new ArrayList<>();
-        for (Student student : studentArrayList) {
-            if (student.getAveragePoint() > 4.5 && student.getAveragePoint() <= 6) {
-                students.add(student);
-            }
-        }
-        return students;
-    }
-
-    public ArrayList<Student> displayAllStudentWeak() {
-        ArrayList<Student> students = new ArrayList<>();
-        for (Student student : studentArrayList) {
-            if (student.getAveragePoint() <= 4.5) {
-                students.add(student);
-            }
-        }
-        return students;
-    }
-
-    public void exportCsv() {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/BaiTapThemNgay29Thang12/test.csv"))) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("Tên,");
-            sb.append("Tuổi,");
-            sb.append("Giới tính,");
-            sb.append("Địa chỉ,");
-            sb.append("Điểm trung bình");
-            sb.append("\n");
-
-            if (studentArrayList.size() > 0) {
-                for (Student student : studentArrayList) {
-                    sb.append(student.getName());
-                    sb.append(",");
-                    sb.append(student.getAge());
-                    sb.append(",");
-                    sb.append(student.getGender());
-                    sb.append(",");
-                    sb.append(student.getAddress());
-                    sb.append(",");
-                    sb.append(student.getAveragePoint());
-                    sb.append("\n");
-                }
-            }
-            bufferedWriter.write(sb.toString());
-            System.out.println("Write successfully");
-        } catch (Exception e) {
+            bufferedWriter.close();
+            System.out.println("Ghi file thành công!");
+        } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    public ArrayList<Student> readFile(String path) {
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+        File file = new File(PATH_NAME);
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] strings = line.split(", ");
+                studentArrayList.add(new Student(strings[0], Integer.parseInt(strings[1]), strings[2], strings[3], Double.parseDouble(strings[4])));
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return studentArrayList;
     }
 }
 
